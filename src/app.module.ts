949bbb8bb793ebcd6a64d2e03ad75dfd7ef817f7
roseaugusto <rose.augusto@sun-asterisk.com>
2025-06-04 10:24:31 +0800

    Added CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './user.service';
+import { PostsService } from './post.service';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot({isGlobal: true, envFilePath:'.env',})],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService, AppService, UsersService, PostsService],
 })
 export class AppModule {}
